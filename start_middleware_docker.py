5f96719c46fe471d1357c881e151648e92210544
zhengwei <1762389546@qq.com>
2024-10-02 21:10:34 +0800

    new

--- start_middleware_docker.py
+++ start_middleware_docker.py
@@ -4,9 +4,9 @@ def run_docker_compose(compose_file_path):
     command = f"docker compose -f {compose_file_path} up -d"
     try:
         subprocess.run(command, shell=True, check=True)
-        print(f"Successfully started services defined in {compose_file_path}")
+        print(f"已成功启动middleware中定义的服务")
     except subprocess.CalledProcessError as e:
-        print(f"Error starting services: {e}")
+        print(f"启动服务时出错: {e}")
 
 if __name__ == "__main__":
     compose_file_path = "./docker-compose.middleware.yaml"  # 替换为你的 docker-compose.yml 路径
